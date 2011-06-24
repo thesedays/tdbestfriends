296491905508ed9030af1aa48c4de93fc4210a62
Keegan Street <keeganstreet@gmail.com>
2011-06-24 16:43:39 +0200

    finished i think

--- tdbestfriends.js
+++ tdbestfriends.js
@@ -145,17 +145,95 @@ var TDBestFriends = (function() {
 
 	// Read the news feed and figure out who the best friends are
 	calculateBestFriends = function(callback) {
-		var points = [], bestFriends = [], i, iLen, post;
+		var points = [], pointsById = {}, friend, bestFriends = [], i, iLen, post, j, jLen, comment, sortByPoints, complete, padOutResults;
 
 		for (i = 0, iLen = feed.length; i < iLen; i += 1) {
 			post = feed[i];
 			if (post.from) {
-				points[post.from.id] = (points[post.from.id] || 0) + options.pointsWallPost;
-				log("Incremented points for " + post.from.name + " to " + points[post.from.id]);
+				friend = pointsById[post.from.id];
+				if (friend) {
+					friend.points += options.pointsWallPost;
+				} else {
+					friend = { id: post.from.id, points: options.pointsWallPost };
+					pointsById[friend.id] = points[points.push(friend) - 1];
+				}
+				//log("Incremented " + post.from.name + "'s points for a wall post to " + friend.points);
+			}
+			if (post.comments && post.comments.data) {
+				for (j = 0, jLen = post.comments.data.length; j < jLen; j += 1) {
+					comment = post.comments.data[j];
+					if (comment.from) {
+						friend = pointsById[comment.from.id];
+						if (friend) {
+							friend.points += options.pointsWallPost;
+						} else {
+							friend = { id: comment.from.id, points: options.pointsComment };
+							pointsById[friend.id] = points[points.push(friend) - 1];
+						}
+						//log("Incremented " + comment.from.name + "'s points for a comment to " + friend.points);
+					}
+				}
+			}
+		}
+		pointsById = null;
+
+		// Sort the friends with points
+		sortByPoints = function(a, b) {
+			if (a.points < b.points) {
+				return 1;
+			} else if (a.points > b.points) {
+				return -1;
+			} else {
+				return 0;
+			}
+		};
+		points.sort(sortByPoints);
+
+		complete = function() {
+			callback.apply(this, [bestFriends]);
+		};
+
+		// Add the top scoring people to the bestFriendsArray if they are friends with the user
+		for (i = 0, iLen = points.length; i < iLen; i += 1) {
+			if (points[i].id !== uid) {
+				for (j = 0, jLen = friends.length; j < jLen; j += 1) {
+					if (points[i].id === friends[j].id) {
+						bestFriends.push(friends[j]);
+						if (bestFriends.length === options.numToReturn) {
+							return complete();
+						}
+						break;
+					}
+				}
 			}
 		}
 
-		callback.apply(this, [friends.slice(0, options.numToReturn)]);
+		// Add some extra random friends to pad out the result list if necessary
+		padOutResults = function() {
+			var unusedFriends = [], i, iLen, j, jLen, found, rand;
+			// Generate a list of unused friends
+			for (i = 0, iLen = friends.length; i < iLen; i += 1) {
+				found = false;
+				for (j = 0, jLen = bestFriends.length; j < jLen; j += 1) {
+					if (friends[i].id === bestFriends[j].id) {
+						found = true;
+						break;
+					}
+				}
+				if (!found) {
+					unusedFriends.push(friends[i]);
+				}
+			}
+			// Add as many unused friends as we need
+			while (bestFriends.length < options.numToReturn) {
+				rand = Math.floor(Math.random() * unusedFriends.length);
+				friend = unusedFriends.splice(rand, 1);
+				bestFriends.push(friend[0]);
+			}
+		};
+		padOutResults();
+
+		return complete();
 	};
 
 	log = function() {
